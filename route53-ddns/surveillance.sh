a8298012759ccd4eaf1669a80314db1579a4acba
fylufox <fylufox@gmail.com>
2025-04-06 03:48:14 +0900

    Modified directory creation and configuration file paths to store temporary script files in appropriate locations.

--- route53-ddns/surveillance.sh
+++ route53-ddns/surveillance.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-cd /root/route53-ddns/
+# ####################
+# make directory.
+# ####################
+if [ ! -d "/var/local/route53-ddns" ]; then
+  mkdir -p /var/local/route53-ddns
+fi
+if [ ! -d "/tmp/route53-ddns/" ]; then
+  mkdir -p /tmp/route53-ddns
+fi
+
 # ####################
 # read config.
 # ####################
-awsprofile=$(sed -ne '/^AwsProfile/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
-RecordName=$(sed -ne '/^RecordName/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
-HostedZoneId=$(sed -ne '/^HostedZoneId/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
+awsprofile=$(sed -ne '/^AwsProfile/p' /etc/route53-ddns.conf | sed -ne 's/^.\+=//gp')
+RecordName=$(sed -ne '/^RecordName/p' /etc/route53-ddns.conf | sed -ne 's/^.\+=//gp')
+HostedZoneId=$(sed -ne '/^HostedZoneId/p' /etc/route53-ddns.conf | sed -ne 's/^.\+=//gp')
 
 # ####################
 # Scripts.
 # ####################
-touch ip
+touch /var/local/route53-ddns/ip
 cip=$(curl ifconfig.io)
 fip=$(cat ip)
 
@@ -21,10 +30,10 @@ fi
 if [[ $cip = $fip ]]; then
   echo matche!
 else
-  echo $cip > ip
-  cip=$(perl -pe 's/\n/ /' ip)
+  echo $cip > /var/local/route53-ddns/ip
+  cip=$(perl -pe 's/\n/ /' /var/local/route53-ddns/ip)
 
-  cat << EOJ > "json"
+  cat << EOJ > "/tmp/route53-ddns/route53.json"
   {
     "Comment" : "",
     "Changes" : [
@@ -45,6 +54,6 @@ else
   }
 EOJ
 
-  aws route53 change-resource-record-sets --hosted-zone-id $HostedZoneId --change-batch file://json $awsprofile
-  rm json
+  aws route53 change-resource-record-sets --hosted-zone-id $HostedZoneId --change-batch file:///tmp/route53-ddns/route53.json $awsprofile
+  rm /tmp/route53-ddns/route53.json
 fi
