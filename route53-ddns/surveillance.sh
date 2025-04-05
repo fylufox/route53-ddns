4cbfc409baa9b8dbb162ee1f4b53d0e8ef47777a
fylufox <fylufox@gmail.com>
2025-04-06 03:13:41 +0900

    Improved configuration loading in surveillance.sh and added conditional configuration for awsprofile.

--- route53-ddns/surveillance.sh
+++ route53-ddns/surveillance.sh
@@ -3,9 +3,9 @@ cd /root/route53-ddns/
 # ####################
 # read config.
 # ####################
-awsprofile=$(sed -ne '/AwsProfile/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
-RecordName=$(sed -ne '/RecordName/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
-HostedZoneId=$(sed -ne '/HostedZoneId/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
+awsprofile=$(sed -ne '/^AwsProfile/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
+RecordName=$(sed -ne '/^RecordName/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
+HostedZoneId=$(sed -ne '/^HostedZoneId/p' route53-ddns.conf | sed -ne 's/^.\+=//gp')
 
 # ####################
 # Scripts.
@@ -13,6 +13,10 @@ HostedZoneId=$(sed -ne '/HostedZoneId/p' route53-ddns.conf | sed -ne 's/^.\+=//g
 touch ip
 cip=$(curl ifconfig.io)
 fip=$(cat ip)
+
+if [[ -n $awsprofile ]]; then
+  awsprofile="--profile ${awsprofile}"
+fi
 
 if [[ $cip = $fip ]]; then
   echo matche!
@@ -41,6 +45,6 @@ else
   }
 EOJ
 
-  aws route53 change-resource-record-sets --hosted-zone-id "${HostedZoneId}" --change-batch file://json --profile "${awsprofile}"
+  aws route53 change-resource-record-sets --hosted-zone-id $HostedZoneId --change-batch file://json $awsprofile
   rm json
 fi
